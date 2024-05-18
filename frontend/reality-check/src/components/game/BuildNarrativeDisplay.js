45e713bac2d9a0f696c6626b9c2393c36ca0c352
tom-plant <tjplantt@gmail.com>
2024-05-18 10:17:36 +0300

    Added feature to bar proceeding through BUILD_NARRATIVE on second runthrough unless selections are changed

--- frontend/reality-check/src/components/game/BuildNarrativeDisplay.js
+++ frontend/reality-check/src/components/game/BuildNarrativeDisplay.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useTranslation } from 'react-i18next'; 
 import { useGameState, useGameDispatch, useGameFunction } from '../../contexts/GameContext';
 import StratBox from '../common/StratBox'; 
@@ -6,20 +6,44 @@ import Counter from '../common/Counter';
 import './BuildNarrativeDisplay.css'; 
 
 const BuildNarrativeDisplay = () => {
+  const { strats, selectedStrat, selectedActor, inCoda, introduceEventVisits } = useGameState();
   const { t } = useTranslation();
   const { buildAndSetNarrative } = useGameFunction();
-  const { strats, selectedStrat, selectedActor } = useGameState();
   const dispatch = useGameDispatch();
   const [buttonClicked, setButtonClicked] = useState(false); 
+  const [initialSelections, setInitialSelections] = useState({
+    actor: null,
+    strats: [],
+  });
 
-// Generate narratives and change view, assuring button can only be clicked once
-const handleGenerateNarrative = async () => {
-    if (!buttonClicked) { 
-        setButtonClicked(true); 
-        dispatch({ type: 'SET_CURRENT_VIEW', payload: 'SELECT_NARRATIVES' });
-        await buildAndSetNarrative(selectedActor, selectedStrat); 
+  // Set initial selections on first render
+  useEffect(() => {
+    if (!initialSelections.actor && !initialSelections.strats.length) {
+      setInitialSelections({
+        actor: selectedActor,
+        strats: selectedStrat,
+      });
     }
-    };
+  }, [selectedActor, selectedStrat, initialSelections]);
+
+  // Generate narratives and change view, assuring button can only be clicked once
+  const handleGenerateNarrative = async () => {
+    if (!buttonClicked) {
+      if (introduceEventVisits === 1 && !inCoda) {
+        const actorChanged = initialSelections.actor !== selectedActor;
+        const stratsChanged = selectedStrat.some((strat, index) => strat !== initialSelections.strats[index]);
+        
+        if (!actorChanged && !stratsChanged) {
+          alert("You must change your selections before proceeding");
+          return;
+        }
+      }
+
+      setButtonClicked(true); 
+      dispatch({ type: 'SET_CURRENT_VIEW', payload: 'SELECT_NARRATIVES' });
+      await buildAndSetNarrative(selectedActor, selectedStrat); 
+    }
+  };
   
     const isButtonEnabled = selectedActor && Array.isArray(selectedStrat) && selectedStrat.length === 2;
     
